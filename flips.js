d6d05e9b3c82cb11b832e76c60162c34358b730e
ianwash <ianwash@live.unc.edu>
2022-02-15 18:44:25 -0500

    okay i think this will work

--- flips.js
+++ flips.js
@@ -1,6 +1,7 @@
 import { coinFlips } from "./modules/coin.mjs";
 import { createRequire } from 'module';
 import { coinFlip } from "./modules/coin.mjs";
+import { countFlips } from "./modules/coin.mjs";
 
 const require = createRequire(import.meta.url);
 const args = require('yargs').argv;
@@ -13,3 +14,4 @@ if (typeof args.number == "undefined") {
     flips = coinFlips(args.number);
 }
 console.log(flips);
+console.log(countFlips(flips));

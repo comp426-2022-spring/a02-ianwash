9d14a6a99b73fb2ca19e142a491d66a3c0fc2255
ianwash <ianwash@live.unc.edu>
2022-02-15 18:54:08 -0500

    Update guess-flip.js

--- guess-flip.js
+++ guess-flip.js
@@ -7,9 +7,5 @@ const args = require('yargs').argv;
 if (typeof args.call === "undefined") {
     console.log("Error: no input.");
 } else {
-    if (args.call !== "heads" || args.call !== "tails") {
-        console.log("Error: not a valid input.");
-    } else {
-        console.log(flipACoin(args.call));
-    }
+    console.log(flipACoin(args.call));
 }

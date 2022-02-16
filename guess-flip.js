0ede077b05afb1724fdd02e7a6b143b6f51e5bea
ianwash <ianwash@live.unc.edu>
2022-02-15 19:00:47 -0500

    okay please!

--- guess-flip.js
+++ guess-flip.js
@@ -4,7 +4,7 @@ import { flipACoin } from './modules/coin.mjs';
 const require = createRequire(import.meta.url);
 const args = require('yargs').argv;
 
-if (typeof args.call === "undefined") {
+if (typeof args.call === "undefined" || (args.call !== "heads" && args.call !== "tails")) {
     console.log("Error: no input.");
 } else {
     console.log(flipACoin(args.call));
